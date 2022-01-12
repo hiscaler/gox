24a2c2667221d1527567d4f2ff0a4e30223eecbf
hiscaler <hiscaler@gmail.com>
2022-01-12 13:47:09 +0800

    ...

--- stringx/string.go
+++ stringx/string.go
@@ -1,5 +1,7 @@
 package stringx
 
+import "strings"
+
 // ToNumber 字符串转换为唯一数字
 // https://stackoverflow.com/questions/5459436/how-can-i-generate-a-unique-int-from-a-unique-string
 func ToNumber(s string) int {
@@ -13,4 +15,30 @@ func ToNumber(s string) int {
 		number += ((x << 16) | (x >> 16)) ^ int(r)
 	}
 	return number
+}
+
+// StringIn 判断 s 是否在 ss 中（忽略大小写）
+func StringIn(s string, ss ...string) bool {
+	if len(ss) == 0 {
+		return false
+	}
+	for _, s2 := range ss {
+		if strings.EqualFold(s, s2) {
+			return true
+		}
+	}
+	return false
+}
+
+// IntIn 判断 i 是否在 ii 中
+func IntIn(i string, ii ...string) bool {
+	if len(ii) == 0 {
+		return false
+	}
+	for _, j := range ii {
+		if i == j {
+			return true
+		}
+	}
+	return false
 }
