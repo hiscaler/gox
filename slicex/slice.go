3f112fdc6632cd1cfb4f190f41e67b485387b143
hiscaler <hiscaler@gmail.com>
2022-01-12 16:15:52 +0800

    ...

--- slicex/slice.go
+++ slicex/slice.go
@@ -1,7 +1,20 @@
 package slicex
 
-// ToInterfaceSlice Change values to interface slice
-func ToInterfaceSlice(values ...interface{}) []interface{} {
+// StringSliceToInterfaceSlice Change string slice to interface slice
+func StringSliceToInterfaceSlice(values []string) []interface{} {
+	if values == nil {
+		return make([]interface{}, 0)
+	}
+	is := make([]interface{}, len(values))
+	for i, value := range values {
+		is[i] = value
+	}
+
+	return is
+}
+
+// IntSliceToInterfaceSlice Change int slice to interface slice
+func IntSliceToInterfaceSlice(values []int) []interface{} {
 	is := make([]interface{}, len(values))
 	for i, value := range values {
 		is[i] = value
