f002e8f58cb556f94d4587e910ec195c0f91ed5b
hiscaler <hiscaler@gmail.com>
2022-01-13 14:14:45 +0800

    ...

--- slicex/slice.go
+++ slicex/slice.go
@@ -1,5 +1,7 @@
 package slicex
 
+import "strings"
+
 // StringToInterface Change string slice to interface slice
 func StringToInterface(values []string) []interface{} {
 	if values == nil {
@@ -21,4 +23,69 @@ func IntToInterface(values []int) []interface{} {
 	}
 
 	return is
+}
+
+// StringSliceEqual Check a, b is equal
+func StringSliceEqual(a, b []string, ignoreCase, ignoreEmpty, trim bool) bool {
+	if ignoreCase || ignoreEmpty || trim {
+		fixFunc := func(ss []string) []string {
+			if len(ss) == 0 {
+				return ss
+			}
+			values := make([]string, 0)
+			for _, s := range ss {
+				if trim {
+					s = strings.TrimSpace(s)
+				}
+				if s == "" && ignoreEmpty {
+					continue
+				}
+				if ignoreCase {
+					s = strings.ToUpper(s)
+				}
+				values = append(values, s)
+			}
+			return values
+		}
+		a = fixFunc(a)
+		b = fixFunc(b)
+	}
+	if len(a) != len(b) {
+		return false
+	}
+
+	for _, av := range a {
+		exists := false
+		for _, bv := range b {
+			if av == bv {
+				exists = true
+				break
+			}
+		}
+		if !exists {
+			return false
+		}
+	}
+	return true
+}
+
+// IntSliceEqual Check a, b is equal
+func IntSliceEqual(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for _, av := range a {
+		exists := false
+		for _, bv := range b {
+			if av == bv {
+				exists = true
+				break
+			}
+		}
+		if !exists {
+			return false
+		}
+	}
+	return true
 }
---
+++ slicex/slice_test.go
@@ -0,0 +1,56 @@
+package slicex
+
+import (
+	"testing"
+)
+
+func TestStringSliceEqual(t *testing.T) {
+	testCases := []struct {
+		A           []string
+		B           []string
+		IgnoreCase  bool
+		IgnoreEmpty bool
+		Trim        bool
+		Except      bool
+	}{
+		{[]string{}, []string{}, true, true, true, true},
+		{[]string{"a", "b", "c"}, []string{"a", "b", "c"}, false, false, true, true},
+		{[]string{"a", "b", "c"}, []string{"a", "b   ", "     c"}, false, false, true, true},
+		{[]string{"a", "b", "c"}, []string{"a", "b   ", "     c"}, false, false, false, false},
+		{[]string{"a", "b", "c", ""}, []string{"a", "b   ", "     c"}, false, false, true, false},
+		{[]string{"a", "b", "c", ""}, []string{"a", "b   ", "     c"}, false, true, true, true},
+		{[]string{"a", "b", "c", ""}, []string{"a", "b   ", "     c", ""}, false, false, true, true},
+		{[]string{"A", "B", "C"}, []string{"a", "b", "c"}, false, true, true, false},
+		{[]string{"A", "B", "C"}, []string{"a", "b", "c"}, true, true, true, true},
+		{[]string{"A", "B", "C"}, []string{"b", "c", "a"}, true, true, true, true},
+		{[]string{"   ", "", " "}, []string{""}, true, true, true, true},
+	}
+	for i, testCase := range testCases {
+		equal := StringSliceEqual(testCase.A, testCase.B, testCase.IgnoreCase, testCase.IgnoreEmpty, testCase.Trim)
+		if equal != testCase.Except {
+			t.Errorf("%d except %v actual %v", i, testCase.Except, equal)
+		}
+	}
+}
+
+func TestIntSliceEqual(t *testing.T) {
+	testCases := []struct {
+		A      []int
+		B      []int
+		Except bool
+	}{
+		{[]int{}, []int{}, true},
+		{[]int{0, 1, 2}, []int{0, 1, 2}, true},
+		{[]int{0, 1, 2}, []int{2, 1, 0}, true},
+		{[]int{0, 1, 2}, []int{1, 2}, false},
+		{[]int{0, 1, 1, 2}, []int{0, 1, 2}, false},
+		{[]int{0, 1, 1, 2}, []int{0, 1, 2, 1}, true},
+	}
+
+	for i, testCase := range testCases {
+		equal := IntSliceEqual(testCase.A, testCase.B)
+		if equal != testCase.Except {
+			t.Errorf("%d except %v actual %v", i, testCase.Except, equal)
+		}
+	}
+}
