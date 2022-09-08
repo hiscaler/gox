a15d0da79e2e465519405984fd3790056216077a
hiscaler <hiscaler@gmail.com>
2022-09-08 11:00:25 +0800

    ...

--- slicex/slice.go
+++ slicex/slice.go
@@ -130,6 +130,30 @@ func IntSliceReverse(ss []int) []int {
 	return vv
 }
 
+// Diff return a slice in ss[0] and not in ss[1:]
+func Diff[T comparable](values ...[]T) []T {
+	diffValues := make([]T, 0)
+	n := len(values)
+	if n == 0 || values[0] == nil {
+		return diffValues
+	} else if n == 1 {
+		return values[0]
+	} else {
+		items := make(map[T]struct{}, 0)
+		for _, vs := range values[1:] {
+			for _, v := range vs {
+				items[v] = struct{}{}
+			}
+		}
+		for _, v := range values[0] {
+			if _, ok := items[v]; !ok {
+				diffValues = append(diffValues, v)
+			}
+		}
+	}
+	return diffValues
+}
+
 func StringSliceDiff(ss ...[]string) []string {
 	diffValues := make([]string, 0)
 	if len(ss) == 0 || ss[0] == nil {
@@ -159,27 +183,5 @@ func StringSliceDiff(ss ...[]string) []string {
 }
 
 func IntSliceDiff(ss ...[]int) []int {
-	diffValues := make([]int, 0)
-	if len(ss) == 1 {
-		diffValues = ss[0]
-	} else if len(ss) > 1 {
-		for _, v1 := range ss[0] {
-			exists := false
-			for _, items := range ss[1:] {
-				for _, v2 := range items {
-					if v1 == v2 {
-						exists = true
-						break
-					}
-				}
-				if exists {
-					break
-				}
-			}
-			if !exists {
-				diffValues = append(diffValues, v1)
-			}
-		}
-	}
-	return diffValues
+	return Diff(ss...)
 }
